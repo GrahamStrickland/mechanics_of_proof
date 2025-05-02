d3bdd5ef08b9efb2e09e08d287045623e6be8171
Graham Strickland <grahamhstrickland@gmail.com>
2025-05-02 15:37:39 +0200

    Add examples 1.2.2 and 1.2.3

--- MechanicsOfProof/Ch01/ProvingInequalities.lean
+++ MechanicsOfProof/Ch01/ProvingInequalities.lean
@@ -6,3 +6,18 @@ example {a b : ℚ} (h1 : a - b = 4) (h2 : a * b = 1) : (a + b) ^2 = 20 :=
     (a + b) ^ 2 = (a - b) ^ 2 + 4 * (a * b) := by ring
     _ = 4 ^ 2 + 4 * 1 := by rw [h1, h2]
     _ = 20 := by ring
+
+example {r s : ℝ} (h1 : s = 3) (h2 : r + 2 * s = -1) : r = -7 := by
+  calc
+    r = r + 2 * s - 2 * s := by ring
+    _ = -1 - 2 * s := by rw [h2]
+    _ = -1 - 2 * 3 := by rw [h1]
+    _ = -7 := by ring
+
+example {a b m n : ℤ} (h1 : a * m + b * n = 1) (h2 : b ^ 2 = 2 * a ^ 2) :
+    (2 * a * n + b * m) ^ 2 = 2 :=
+  calc
+    (2 * a * n + b * m) ^ 2
+      = 2 * (a * m + b * n) ^ 2 + (m ^ 2 - 2 * n ^ 2) * (b ^ 2 - 2 * a ^ 2) := by ring
+    _ = 2 * 1 ^ 2 + (m ^ 2 - 2 * n ^ 2) * (2 * a ^ 2 - 2 * a ^ 2) := by rw [h2, h1]
+    _ = 2 := by ring
