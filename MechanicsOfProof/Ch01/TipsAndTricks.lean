997ffdf20fea857899b1782fe436dc6cc2719941
Graham Strickland <grahamhstrickland@gmail.com>
2025-05-05 22:01:29 +0200

    Add exercises 1.3.11.1-1.3.11.4

--- MechanicsOfProof/Ch01/TipsAndTricks.lean
+++ MechanicsOfProof/Ch01/TipsAndTricks.lean
@@ -64,3 +64,29 @@ example {z : ℝ} (h1 : z ^ 2 - 2 = 0) : z ^ 4 - z ^ 3 - z ^ 2 + 2 * z + 1 = 3 :
     z ^ 4 - z ^ 3 - z ^ 2 + 2 * z + 1 = (z ^ 2 - z + 1) * (z ^ 2 - 2) + 3 := by ring
     _ = (z ^ 2 - z + 1) * 0 + 3 := by rw[h1]
     _ = 3 := by ring
+
+example {x y : ℝ} (h1 : x = 3) (h2 : y = 4 * x - 3) : y = 9 :=
+  calc
+    y = 4 * x - 3 := by rw[h2]
+    _ = 4 * 3 - 3 := by rw[h1]
+    _ = 9 := by ring
+
+example {a b : ℤ} (h : a - b = 0) : a = b :=
+  calc
+    a = a - b + b := by ring
+    _ = 0 + b := by rw[h]
+    _ = b := by ring
+
+example {x y : ℤ} (h1 : x - 3 * y = 5) (h2 : y = 3) : x = 14 :=
+  calc
+    x = x - 3 * y + 3 * y := by ring
+    _ = 5 + 3 * y := by rw[h1]
+    _ = 5 + 3 * 3 := by rw[h2]
+    _ = 14 := by ring
+
+example {p q : ℚ} (h1 : p - 2 * q = 1) (h2 : q = -1) : p = -1 :=
+  calc
+    p = p - 2 * q + 2 * q := by ring
+    _ = 1 + 2 * q := by rw[h1]
+    _ = 1 + 2 * (-1) := by rw[h2]
+    _ = -1 := by ring
