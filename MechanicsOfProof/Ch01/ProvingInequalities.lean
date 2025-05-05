033b1325f6ad50d5cc1aeb01fc5f0aeaaee330a7
Graham Strickland <grahamhstrickland@gmail.com>
2025-05-05 20:48:56 +0200

    Add example 1.2.4

--- MechanicsOfProof/Ch01/ProvingInequalities.lean
+++ MechanicsOfProof/Ch01/ProvingInequalities.lean
@@ -21,3 +21,13 @@ example {a b m n : ℤ} (h1 : a * m + b * n = 1) (h2 : b ^ 2 = 2 * a ^ 2) :
       = 2 * (a * m + b * n) ^ 2 + (m ^ 2 - 2 * n ^ 2) * (b ^ 2 - 2 * a ^ 2) := by ring
     _ = 2 * 1 ^ 2 + (m ^ 2 - 2 * n ^ 2) * (2 * a ^ 2 - 2 * a ^ 2) := by rw [h2, h1]
     _ = 2 := by ring
+
+example {a b c d e f : ℤ} (h1 : a * d = b * c) (h2 : c * f = d * e) :
+  d * (a * f - b * e) = 0 :=
+  calc
+    d * (a * f - b * e)
+      = (a * d) * f - d * b * e := by ring
+    _ = (b * c) * f - d * b * e := by rw [h1]
+    _ = b * (c * f) - d * b * e := by ring
+    _ = b * (d * e) - d * b * e := by rw [h2]
+    _ = 0 := by ring
